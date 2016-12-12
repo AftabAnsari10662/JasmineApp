afbf95f6f0422e429f70e8f0b0c4860774017e55
Aftab Ansari <AftabAnsari10662@gmail.com>
2016-12-12 08:01:08 -0800

    controller Test assed;

--- src/app/message.controller.spec.js
+++ src/app/message.controller.spec.js
@@ -4,19 +4,25 @@ describe("Message Controller", function () {
 
     var mockController;
     var $controller;
+    var $httpBackend;
     var messageService;
     var $scope = {};
     var $rootScope;
-    beforeEach(inject(function (_$controller_, _$rootScope_, _messageService_) {
+    var authService;
+
+    beforeEach(inject(function (_$controller_, _$rootScope_, _messageService_, _$httpBackend_, _authService_) {
         $controller = _$controller_;
         $rootScope = _$rootScope_;
         messageService = _messageService_;
+        $httpBackend = _$httpBackend_;
+        authService = _authService_;
     }))
 
     it("Getting Message", function () {
 
         var ctrl = $controller("messageController", { $scope: $scope });
-        expect(ctrl).toBeDefined();
-        expect(ctrl).toBe("NNN");
+        $rootScope.$apply();
+        //expect(ctrl).toBeDefined();
+        expect($scope.message).toBe("Hello Jasmine!");
     })
 })
